64c8a61dc7e2867919b72edcf42091bd2b9d922c
kghaby <kyleghaby@gmail.com>
2024-08-06 23:44:23 -0500

    crimson pro and fira code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,11 @@
  */
 
 // You can delete this file if you're not using it
+
+// Supports weights 200-900
+import "@fontsource-variable/crimson-pro";
+import '@fontsource-variable/crimson-pro/wght-italic.css';
+
+// Supports weights 300-700
+import '@fontsource-variable/fira-code';
+
