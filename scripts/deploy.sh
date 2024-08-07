3587e751ebae6a5cb0db8b1363570db841cf36d5
kghaby <kyleghaby@gmail.com>
2024-08-06 23:45:11 -0500

    0.3.0

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # https://github.com/gatsbyjs/gatsby-ru/blob/master/docs/docs/how-gatsby-works-with-github-pages.md#deploying-to-a-path-on-github-pages
 
+gatsby clean
 npm run deploy
 
 # In repo Settings>Pages: deploy from gh-pages branch 
