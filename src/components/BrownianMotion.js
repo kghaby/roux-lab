616858beb87f548e36bacfa9b79c1d2aa6526826
kghaby <kyleghaby@gmail.com>
2024-08-09 04:33:59 -0500

    brownian

---
+++ src/components/BrownianMotion.js
@@ -0,0 +1,152 @@
+import React, { useEffect, useRef, useState } from 'react';
+import "../components/layout.css"; // global styles
+
+const BrownianMotion = ({ D, F, T, dt, particleDensity }) => {
+  const canvasRef = useRef(null);
+  const [isPaused, setIsPaused] = useState(false);
+  const [mousePos, setMousePos] = useState({ x: -1, y: -1 }); 
+  const [particles, setParticles] = useState([]); 
+  const animationFrameIdRef = useRef(null); 
+
+  // Ensure canvas dimensions are set before initializing particles
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }, []);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const numParticles = Math.ceil(Math.sqrt(Math.floor((canvas.width * canvas.height)) / 10000) * particleDensity);
+    console.log("numParticles:", numParticles);
+    const initParticles = Array.from({ length: numParticles }, (_, i) => ({
+      x: Math.random() * canvas.width,
+      y: Math.random() * canvas.height,
+      opacity: Math.random() * 0.2 + 0.3,
+    }));
+    
+    setParticles(initParticles);
+  }, [particleDensity]);
+
+  // Main animation loop
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    const fontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+    const offset = (0.5 * fontSize) ** 2; // biggest particle
+
+    const draw = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      particles.forEach(particle => {
+        if (!isPaused) {
+          // Calculate the potential well force if near the mouse
+          const dx = mousePos.x - particle.x;
+          const dy = mousePos.y - particle.y;
+          const distanceSquared = dx * dx + dy * dy;
+
+          let forceX = 0;
+          let forceY = 0;
+
+          if (distanceSquared < 100000 && mousePos.x >= 0 && mousePos.y >= 0) {
+            const distance = Math.sqrt(distanceSquared);
+            forceX = (dx / distance) * F;
+            forceY = (dy / distance) * F;
+          }
+          particle.x += (Math.random() - 0.5) * 2 * Math.sqrt(2 * D * dt) + forceX * dt;
+          particle.y += (Math.random() - 0.5) * 2 * Math.sqrt(2 * D * dt) + forceY * dt;
+
+          // Smooth edge wrapping
+          if (particle.x > canvas.width + offset) particle.x = -offset;
+          if (particle.x < -offset) particle.x = canvas.width + offset;
+          if (particle.y > canvas.height + offset) particle.y = -offset;
+          if (particle.y < -offset) particle.y = canvas.height + offset;
+        }
+
+        // Draw the particle
+        ctx.beginPath();
+        ctx.arc(particle.x, particle.y, (particle.opacity * fontSize) ** 2, 0, 2 * Math.PI);
+        ctx.fillStyle = `rgba(118, 30, 30, ${particle.opacity})`;
+        ctx.fill();
+      });
+
+      animationFrameIdRef.current = requestAnimationFrame(draw);
+    };
+
+    draw();
+
+    return () => {
+      cancelAnimationFrame(animationFrameIdRef.current);
+    };
+  }, [isPaused, D, F, T, dt, particles, mousePos]);
+
+  // Handle mouse movement with throttling
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    let lastMoveTime = 0;
+    const throttleDelay = 50; // Throttle mouse move events to 20 FPS
+
+    const handleMouseMove = (event) => {
+      const now = Date.now();
+      if (now - lastMoveTime > throttleDelay) {
+        lastMoveTime = now;
+
+        const rect = canvas.getBoundingClientRect();
+        setMousePos({ x: event.clientX - rect.left, y: event.clientY - rect.top });
+      }
+    };
+
+    const handleMouseLeave = () => {
+      setMousePos({ x: -1, y: -1 }); // Reset to out of bounds
+    };
+
+    canvas.addEventListener('mousemove', handleMouseMove);
+    canvas.addEventListener('mouseleave', handleMouseLeave);
+
+    return () => {
+      canvas.removeEventListener('mousemove', handleMouseMove);
+      canvas.removeEventListener('mouseleave', handleMouseLeave);
+    };
+  }, []);
+
+  // Handle canvas resizing without reinitializing particles
+  useEffect(() => {
+    const resizeCanvas = () => {
+      const canvas = canvasRef.current;
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    };
+
+    resizeCanvas();
+    window.addEventListener('resize', resizeCanvas);
+
+    return () => {
+      window.removeEventListener('resize', resizeCanvas);
+    };
+  }, []);
+
+  const handlePause = () => {
+    setIsPaused(!isPaused);
+  };
+
+  return (
+    <canvas
+      ref={canvasRef}
+      onClick={handlePause}
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        zIndex: 0.1,
+        width: '100%',
+        height: '100%',
+        opacity: 0, 
+      }}
+      className="brownian"
+    />
+  );
+};
+
+export default BrownianMotion;
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,21 @@ import * as React from "react";
 import { graphql } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Tilt from 'react-parallax-tilt';
+import { MathComponent } from 'mathjax-react';
 
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import BrownianMotion from "../components/BrownianMotion";
 import * as indexStyles from "../components/index.module.css";
 import "../components/layout.css"; // global styles
 
-// TODO: add timed protein in background. add collage or brownian dyn in background
-
 const IndexPage = ({ data }) => {
+  const [D, setD] = React.useState(10.0);
+  const [F, setF] = React.useState(3);
+  const [T, setT] = React.useState(300);
+  const [dt, setDt] = React.useState(0.1);
+  const [particleDensity, setParticleDensity] = React.useState(4);
+
   const images = data.allFile.nodes
     .map(node => ({
       ...node,
@@ -23,9 +29,16 @@ const IndexPage = ({ data }) => {
 
   return (
     <Layout>
-      <div className="centeredContent">
+      <BrownianMotion
+        D={D}
+        F={F}
+        T={T}
+        dt={dt}
+        particleDensity={particleDensity}
+      />
+      <div className="centeredContent disableClick">
         <div className={indexStyles.firstScreen}>
-          <div className={`${indexStyles.introContainer}`}>
+          <li className={`${indexStyles.introContainer} fadeInBG enableClick glass`}>
             <p className={`${indexStyles.intro} fadeIn1`}>
               Welcome to the laboratory of Benoît Roux, in the
               <a href="https://biochem.uchicago.edu" className={indexStyles.link}> Department of Biochemistry and Molecular Biology</a> at <a href="http://www.uchicago.edu" className={indexStyles.link}> The University of Chicago</a>.
@@ -33,14 +46,14 @@ const IndexPage = ({ data }) => {
             <p className={`${indexStyles.intro} fadeIn2`}>
               Here, you will find information on our research, our publications, current and past group members, and various computational tools for theoretical biophysics.
             </p>
-          </div>
-          <div className={`${indexStyles.mainImageWrapper} fadeIn3`}>
+          </li>
+          <div className={`${indexStyles.mainImageWrapper} fadeIn3 enableClick`}>
             <Tilt
                 tiltReverse={false}
                 tiltMaxAngleX={4}
                 tiltMaxAngleY={4}
                 perspective={900}
-                scale={1.05}
+                scale={1.0}
                 transitionSpeed={1000}
                 reset={true}
                 gyroscope={true}
@@ -52,20 +65,20 @@ const IndexPage = ({ data }) => {
                 alt={`Main Group Photo (${mainImage.year})`}
               />
               <div className={indexStyles.imageOverlay}>
-                <span className={indexStyles.imageText}>{`${mainImage.year}`}</span>
+                <span className={indexStyles.imageText}>{mainImage.year}</span>
               </div>
             </Tilt>
           </div>
         </div>
         <div className={indexStyles.imageScrollContainer}>
           {otherImages.map((image, index) => (
-            <div key={index} className={`${indexStyles.imageWrapper} hidden`}>
+            <div key={index} className={`${indexStyles.imageWrapper} hidden enableClick`}>
               <Tilt
                 tiltReverse={false}
                 tiltMaxAngleX={4}
                 tiltMaxAngleY={4}
                 perspective={900}
-                scale={1.05}
+                scale={1.0}
                 transitionSpeed={1000}
                 reset={true}
                 gyroscope={true}
@@ -82,6 +95,55 @@ const IndexPage = ({ data }) => {
               </Tilt>
             </div>
           ))}
+        </div>
+        <div className="blankPage">
+          <div className="glass enableClick">
+            <div className={indexStyles.equation}>
+              <MathComponent tex={String.raw`x(t + \Delta t) = x(t) + \left(\frac{DF(x(t))}{k_B T}\right)\Delta t + g`} />
+            </div>
+            <div className={indexStyles.slidersContainer}>
+              <label htmlFor="diffusion">D</label>
+              <input
+                id="diffusion"
+                type="range"
+                min="0.1"
+                max="500.0"
+                step="1"
+                value={D}
+                onChange={(e) => setD(parseFloat(e.target.value))}
+              />
+              <label htmlFor="force">F</label>
+              <input
+                id="force"
+                type="range"
+                min="0.0"
+                max="20.0"
+                step="0.1"
+                value={F}
+                onChange={(e) => setF(parseFloat(e.target.value))}
+              />
+              {/* <label htmlFor="temperature">T</label>
+              <input
+                id="temperature"
+                type="range"
+                min="100"
+                max="500"
+                step="10"
+                value={T}
+                onChange={(e) => setT(parseFloat(e.target.value))}
+              />
+              <label htmlFor="timestep">Δt</label>
+              <input
+                id="timestep"
+                type="range"
+                min="0.01"
+                max="1.0"
+                step="0.01"
+                value={dt}
+                onChange={(e) => setDt(parseFloat(e.target.value))}
+              /> */}
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
