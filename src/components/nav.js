c7d5394b9bcf133e0bcb60ee5fffbcf2970314b8
kghaby <kyleghaby@gmail.com>
2024-08-14 01:52:24 -0500

    more nav styling

--- src/components/nav.js
+++ src/components/nav.js
@@ -10,34 +10,34 @@ const Nav = forwardRef(({ className }, ref) => {
     <nav className={className} ref={ref}>
       <ul className="nav-list">
         <li className={location.pathname === "/" ? "active-link" : ""}>
-          <Link to="/">Home</Link>
+          <Link to="/"><span className="text-content">Home</span></Link>
         </li>
         <li className={location.pathname === "/members/" ? "active-link" : ""}>
-          <Link to="/members">Members</Link>
+          <Link to="/members"><span className="text-content">Members</span></Link>
         </li>
         <li className={location.pathname === "/research/" ? "active-link" : ""}>
-          <Link to="/research">Research</Link>
+          <Link to="/research"><span className="text-content">Research</span></Link>
         </li>
         <li className={location.pathname === "/methods/" ? "active-link" : ""}>
-          <Link to="/methods">Methods</Link>
+          <Link to="/methods"><span className="text-content">Methods</span></Link>
         </li>
         <li className={location.pathname === "/publications/" ? "active-link" : ""}>
-          <Link to="/publications">Publications</Link>
+          <Link to="/publications"><span className="text-content">Publications</span></Link>
         </li>
         <li className={location.pathname === "/openings/" ? "active-link" : ""}>
-          <Link to="/openings">Openings</Link>
+          <Link to="/openings"><span className="text-content">Openings</span></Link>
         </li>
         <li className={location.pathname === "/gallery/" ? "active-link" : ""}>
-          <Link to="/gallery">Gallery</Link>
+          <Link to="/gallery"><span className="text-content">Gallery</span></Link>
         </li>
         <li className={location.pathname === "/tools/" ? "active-link" : ""}>
-          <Link to="/tools">Tools</Link>
+          <Link to="/tools"><span className="text-content">Tools</span></Link>
         </li>
         <li className={location.pathname === "/useful-links/" ? "active-link" : ""}>
-          <Link to="/useful-links">Useful Links</Link>
+          <Link to="/useful-links"><span className="text-content">Useful Links</span></Link>
         </li>
         <li className={location.pathname === "/contacts/" ? "active-link" : ""}>
-          <Link to="/contacts">Contacts</Link>
+          <Link to="/contacts"><span className="text-content">Contacts</span></Link>
         </li>
       </ul>
     </nav>
